d668bd8bd4e10c73e9d7b56999df298189231929
Reluckycf <reluckycf@users.noreply.github.com>
2025-08-08 21:13:48 -0500

    Changes to the way generation looks, added new buttons and copy by click on the username

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react"
 import { generate } from "./urug";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { faArrowUpRightFromSquare, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
@@ -9,10 +9,12 @@ function App() {
   const [posX, setPosX] = useState(0);
   const [posY, setPosY] = useState(0);
 
-  const [scount, setSCount] = useState(6);
+  const [scount, setSCount] = useState(4);
   const [wcount, setWCount] = useState(4)
   const [gName, setGName] = useState("");
-  const [userName, setUsername] = useState("");
+  const [randomWord, setRandomWord] = useState("");
+  const [generating, setGenerating] = useState(false);
+  const [copied, setCopied] = useState(false);
 
   const [isTracking, setTracking] = useState(false);
   const [isTracking2, setTracking2] = useState(false);
@@ -195,8 +197,7 @@ function App() {
   useEffect(() => {
     const handleKey = (e) => {
       if (e.key === "Enter"){
-        const username = generate(scount)
-        uinput.current.value = username
+        handleClickGenAll()
       }
     }
 
@@ -276,25 +277,76 @@ function App() {
     }
   }
 
-  const handleChange = (e) => {
-    setUsername(e.target.value)
-  }
 
   const handleClickGen = (e) => {
-    const username = generate(scount)
-    setGName(username)
-    uinput.current.value = username
-    setUsername(username)
+    setGenerating(true)
+    let countInt = 0
+    let maxCount = Math.random() * (8 - 4) + 4
+    const interval = setInterval(() => {
+      if(countInt <= maxCount) {
+        const username = generate(scount)
+        setGName(username)
+        countInt++;
+      } else {
+        clearInterval(interval)
+        setGenerating(false)
+      }
+    }, 50)
+  }
+
+  const handleClickGenAll = async () => {
+    setGenerating(true)
+    let countInt = 0
+    let maxCount = Math.random() * (8 - 4) + 4
+    const interval = setInterval(() => {
+      if(countInt <= maxCount) {
+        const username = generate(scount)
+        setGName(username)
+        countInt++;
+      } else {
+        clearInterval(interval)
+        setGenerating(false)
+      }
+    }, 50)
+
+    const res = await fetch('https://random-word-api.vercel.app/api?words=1&length=' + wcount)
+    if(res.ok) {
+      setGenerating(true)
+      const body = await res.json()
+      setRandomWord(body[0])
+      setTimeout(() => {
+        setGenerating(false)
+      }, 400)
+    }
+
   }
 
   const handleClickRandWord = async (e) => {
     const res = await fetch('https://random-word-api.vercel.app/api?words=1&length=' + wcount)
     if(res.ok) {
+      setGenerating(true)
       const body = await res.json()
-      const username = gName + body[0]
-      uinput.current.value = gName + body[0]
-      setUsername(username)
+      setRandomWord(body[0])
+      setTimeout(() => {
+        setGenerating(false)
+      }, 400)
     }
+  }
+
+  const handleCopy = (e) => {
+    if (!gName && !randomWord) {
+      return
+    }
+    navigator.clipboard.writeText(gName+randomWord)
+      .then(() => {
+        setCopied(true);
+        setTimeout(()=> {
+          setCopied(false)
+        }, 3000)
+      })
+      .catch(err => {
+        console.error("Failed to copy text: ", err);
+      });
   }
 
   return (
@@ -306,20 +358,45 @@ function App() {
               <div className="w-full h-full">
                 <div className="relative">
                   <div className="absolute backdrop-blur-2xl bg-black inset-0"></div>
-                  <input onChange={handleChange} ref={uinput} type="text" className="relative text-center w-full outline-none transition text-white border border-pink-500 py-3 px-5 text-3xl" spellCheck={false} placeholder="Generate a username" />
+                  <div className="relative w-full text-white border border-pink-500 py-3 px-5 text-3xl flex justify-center overflow-x-auto">
+                    <p onClick={handleCopy} title="Click to copy" className={`w-full text-center ${gName||randomWord? "cursor-pointer" : ""}`}>
+                      <span id="gName" className={`${gName ? "" : "opacity-70"} ${generating ? "text-pink-500" : "text-white"} transition duration-300`}>{gName ? gName : "Generate a username"}</span>
+                      <span id="randomWord" className={`${generating ? "text-cyan-500" : "text-white"} transition duration-300`}>{randomWord}</span>
+                    </p>
+                  </div>
+                  <div className={`Copied-text ${copied ? "opacity-100" : "opacity-0"} transition duration-1000 select-none pointer-events-none z-10 absolute w-full -bottom-11 flex items-center justify-center`}>
+                    <div className="absolute bg-slate-700 w-4 h-4 rotate-45 -top-1"></div>
+                    <div className="relative bg-slate-700 px-3 py-2 rounded">Username Copied!</div>
+                  </div>
+                  {/* <input onChange={handleChange} ref={uinput} type="text" className="relative text-center w-full outline-none transition text-white border border-pink-500 py-3 px-5 text-3xl" spellCheck={false} placeholder="Generate a username" /> */}
                 </div>
-                <div className="bg-indigo-950 pb-20 pt-10">
+                <div className="bg-indigo-950 pb-20 pt-15">
                   <div className="w-full px-10 flex flex-col">
-                      <div className="flex flex-wrap md:flex-nowrap gap-5 mb-5 mx-auto">
-                        <button onClick={handleClickGen} className="mb-auto justify-self-center px-5 py-3 bg-pink-500 rounded md:text-2xl font-['Oswald'] hover:bg-transparent border-2 cursor-pointer border-transparent transition hover:text-pink-500 hover:border-pink-500">Generate</button>
+                      <div className="flex flex-wrap sm:flex-row flex-col md:flex-nowrap gap-5 mb-5 mx-auto justify-center">
+                        <button onClick={handleClickGen} className="mb-auto justify-self-center px-5 py-3 bg-pink-500 rounded md:text-2xl font-['Oswald'] hover:bg-transparent border-2 cursor-pointer border-transparent transition hover:text-pink-500 hover:border-pink-500">Random Name</button>  
+                        <div className="order-first md:order-none group relative bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-500 mb-auto rounded">
+                            <div className="absolute inset-0 bg-transparent group-hover:bg-indigo-950 rounded m-0.5 transition"></div>
+                            <button onClick={handleClickGenAll} className="relative mb-auto justify-self-center px-5 py-3 bg-gradient-to-r border-2 border-transparent hover:from-pink-500 hover:via-purple-500 hover:to-cyan-500 rounded md:text-2xl font-['Oswald'] cursor-pointer transition hover:text-transparent bg-clip-text ">Generate Username</button>
+                        </div>
                         <div className="flex flex-col">
-                          <button onClick={handleClickRandWord} className="px-5 py-3 bg-cyan-500 rounded md:text-2xl font-['Oswald'] hover:bg-transparent border-2 cursor-pointer border-transparent transition hover:text-cyan-500 hover:border-cyan-500">Add Random Word</button>
+                          <button onClick={handleClickRandWord} className="px-5 py-3 bg-cyan-500 rounded md:text-2xl font-['Oswald'] hover:bg-transparent border-2 cursor-pointer border-transparent transition hover:text-cyan-500 hover:border-cyan-500">Random Word</button>
                           <a className="text-xs underline opacity-50 hover:opacity-80 text-center" href="https://random-word-api.vercel.app" target="_blank" rel="noreferrer">By Rando Vercel API</a>
                         </div>
+                        {
+                          randomWord && (
+                            <button onClick={()=>setRandomWord("")} title="Remove Random Word" className="mb-auto px-5 py-3 bg-cyan-500 rounded md:text-2xl font-['Oswald'] hover:bg-transparent border-2 cursor-pointer border-transparent transition hover:text-cyan-500 hover:border-cyan-500">
+                              <span className="hidden sm:block">
+                                <FontAwesomeIcon icon={faXmark}/>
+                              </span>
+                              <span className="sm:hidden">Remove Word</span>
+                            </button>
+                          )
+                        }
+                        
                       </div>
                       {
-                        userName && (
-                          <a href={"https://instantusername.com/?q=" + encodeURI(userName)} target="_blank" rel="noreferrer" title="Check if the username is available on social media" className="mx-auto mb-10 px-5 py-2 disabled:opacity-60 bg-emerald-500 rounded md:text-2xl font-['Oswald'] hover:bg-transparent border-2 cursor-pointer border-transparent transition hover:text-emerald-500 hover:border-emerald-500">
+                        (gName || randomWord) && (
+                          <a href={"https://instantusername.com/?q=" + encodeURI(gName+randomWord)} target="_blank" rel="noreferrer" title="Check if the username is available on social media" className="mx-auto mb-10 px-5 py-2 disabled:opacity-60 bg-emerald-500 rounded md:text-2xl font-['Oswald'] hover:bg-transparent border-2 cursor-pointer border-transparent transition hover:text-emerald-500 hover:border-emerald-500">
                             <div className="flex gap-2 items-center">
                               <p>Check Username</p>
                               <div className="text-lg">
